80adc353ac584bed61a19f5c2a9df74bbe5cd730
sung.balance <sung.balance@gmail.com>
2025-10-17 06:24:56 +0000

    init

--- tests/module_tests/run_async_copy_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run nsys profile on the async copy test
-nsys profile \
-  --trace=cuda,nvtx,osrt \
-  --output=async_copy_test_profile \
-  --force-overwrite true \
-  python async_copy_test.py
